d02ff1e205ca121f2b6b410ee6d1bfdbd75e1377
azuterios <icakablackberry@gmail.com>
2018-09-20 23:33:45 +0300

    Added Test Question 3
    Powershell file, containing the HTTP Get Rest API Request with data transformation.

---
+++ Get-MovieTitles.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True)]
+   [string]$title
+)
+
+function Get-Titles([string]$title, [int]$pageNumber)
+{
+
+    $webRequest = Invoke-WebRequest -Method Get -Uri https://jsonmock.hackerrank.com/api/movies/search/?Title=$title`&page=$pageNumber | ConvertFrom-Json
+
+    return $webRequest
+
+}
+
+Write-Output "$title"
+Write-Output "----------------------------------------"
+
+$webRequest = Get-Titles -title $title
+$titles = New-Object string[] $webRequest.total
+
+#Initialize an empty array for all the requested pages
+$webRequests=@()
+
+for($i=1; $i -le $webRequest.total_pages; $i++)
+{
+
+    $webRequests += (Get-Titles -title $title -pageNumber $i)
+
+}
+
+
+
+foreach($obj in $webRequests)
+{
+
+    $titles += $obj.data.title
+
+}
+
+$titles | Sort-Object
