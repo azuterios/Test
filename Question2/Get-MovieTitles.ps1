70a6fd9dc604e8e77d09e062971b6fc64541020d
azuterios <fdsfdsfdsfds@lsjhfldshfldsazure>
2018-09-23 13:09:18 +0300

    Added URL Encoding to support additional characters

--- Question2/Get-MovieTitles.ps1
+++ Question2/Get-MovieTitles.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+﻿[CmdletBinding()]
 Param(
    [Parameter(Mandatory=$True)]
    [string]$title
@@ -6,15 +6,17 @@ Param(
 
 function Get-Titles([string]$title, [int]$pageNumber)
 {
-    $webRequest = Invoke-WebRequest -Method Get -Uri https://jsonmock.hackerrank.com/api/movies/search/?Title=$title`&page=$pageNumber | ConvertFrom-Json
+    $URI="https://jsonmock.hackerrank.com/api/movies/search/?Title=$title`&page=$pageNumber"
+    [System.Web.HttpUtility]::UrlEncode($URI) 
+    $webRequest = Invoke-WebRequest -Method Get -Uri $URI | ConvertFrom-Json
 
     return $webRequest
+
 }
 
 Write-Output "$title"
 Write-Output "----------------------------------------"
 
-#Get total pages and initialize titles with total count
 $webRequest = Get-Titles -title $title
 $titles = New-Object string[] $webRequest.total
 
@@ -23,12 +25,18 @@ $webRequests=@()
 
 for($i=1; $i -le $webRequest.total_pages; $i++)
 {
+
     $webRequests += (Get-Titles -title $title -pageNumber $i)
+
 }
+
+
 
 foreach($obj in $webRequests)
 {
+
     $titles += $obj.data.title
+
 }
 
 $titles | Sort-Object
